9bc40c6874efbaae5fe225d6e7c55ebc91669338
john-royal <34844819+john-royal@users.noreply.github.com>
2023-02-25 22:36:10 -0500

    chore: add eslint and prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Frontend
-    </div>
-  );
+  return <div className="App">Frontend</div>;
 }
 
 export default App;
