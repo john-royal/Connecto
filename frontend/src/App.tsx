d9b5a43403ce7c8adf7ebf92a498cc3d41007af4
john-royal <34844819+john-royal@users.noreply.github.com>
2023-03-10 12:04:38 -0500

    chore(frontend): create new frontend with vite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,56 +1,38 @@
-import CssBaseline from '@mui/joy/CssBaseline';
-import { CssVarsProvider, StyledEngineProvider } from '@mui/joy/styles';
-import { PropsWithChildren } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-import LoadingView from './components/LoadingView';
-import { AuthProvider } from './lib/auth';
-import CreateAccount from './routes/auth/CreateAccount';
-import SignIn from './routes/auth/SignIn';
-import SignOut from './routes/auth/SignOut';
-import ChatView from './routes/ChatView';
-import StartView from './routes/StartView';
+function App(): JSX.Element {
+  const [count, setCount] = useState(0)
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <StartView />,
-  },
-  {
-    path: '/chat',
-    element: <ChatView />,
-  },
-  {
-    path: '/sign-in',
-    element: <SignIn />,
-  },
-  {
-    path: '/create-account',
-    element: <CreateAccount />,
-  },
-  {
-    path: '/sign-out',
-    element: <SignOut />,
-  },
-]);
-
-export default function App() {
   return (
-    <StylingProvider>
-      <AuthProvider>
-        <RouterProvider router={router} fallbackElement={<LoadingView />} />
-      </AuthProvider>
-    </StylingProvider>
-  );
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button
+          onClick={() => {
+            setCount((count) => count + 1)
+          }}
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
 
-function StylingProvider({ children }: PropsWithChildren) {
-  return (
-    <StyledEngineProvider injectFirst>
-      <CssVarsProvider>
-        <CssBaseline />
-        {children}
-      </CssVarsProvider>
-    </StyledEngineProvider>
-  );
-}
+export default App
---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true
+  }
+})
