6a5ef1959b2267f2745bc84f12bc909c83962b8f
john-royal <34844819+john-royal@users.noreply.github.com>
2023-03-15 18:42:23 -0400

    fix(frontend): correct problem with admin chat and remove dead code

--- frontend/src/routes/admin/AdminChatView.tsx
+++ frontend/src/routes/admin/AdminChatView.tsx
@@ -1,28 +1,14 @@
-import '../../App.css'
-import SendIcon from '@mui/icons-material/Send'
 import AttachFileIcon from '@mui/icons-material/AttachFile'
-import Box from '@mui/joy/Box'
-import CircularProgress from '@mui/joy/CircularProgress'
-import FormControl from '@mui/joy/FormControl'
-import FormHelperText from '@mui/joy/FormHelperText'
-import IconButton from '@mui/joy/IconButton'
-import Input from '@mui/joy/Input'
-import {
-  Fragment,
-  forwardRef,
-  useEffect,
-  useRef,
-  useState,
-  type FormEventHandler,
-  type Ref
-} from 'react'
+import SendIcon from '@mui/icons-material/Send'
+import { Fragment, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { useAuth } from '../../lib/auth'
-import { useChat, type Message } from '../../lib/chat'
+import '../../App.css'
 import DashboardLayout from '../../layouts/DashboardLayout'
+import { useAuth } from '../../lib/auth'
+import { useChat } from '../../lib/chat'
 
-function MessageContainer(thread) {
-  const { messages, sendMessage } = useChat(Number(thread))
+function MessageContainer({ threadId }: { threadId: number }) {
+  const { messages, sendMessage } = useChat(threadId)
   const { user } = useAuth()
   const [inputValue, setInputValue] = useState('')
 
@@ -111,91 +97,10 @@ function MessageContainer(thread) {
   )
 }
 
-function MessageForm(
-  { onSubmit }: { onSubmit: (message: string) => Promise<void> },
-  ref?: Ref<HTMLFormElement>
-) {
-  const [message, setMessage] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-
-  const handleSubmit: FormEventHandler = (e) => {
-    e.preventDefault()
-    setLoading(true)
-    onSubmit(message)
-      .then(() => {
-        setMessage('')
-      })
-      .catch((error) => {
-        setError(error.message)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }
-
-  return (
-    <form onSubmit={handleSubmit} ref={ref}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          gap: 1.5,
-          mt: 2,
-          overflowAnchor: 'auto'
-        }}
-      >
-        <FormControl sx={{ flex: 1 }}>
-          <Input
-            sx={{ flex: 1 }}
-            name="message"
-            placeholder="Type your message"
-            value={message}
-            onChange={(e) => {
-              setMessage(e.target.value)
-            }}
-            disabled={loading}
-            error={error.length > 0}
-            endDecorator={
-              <IconButton
-                variant="soft"
-                color="primary"
-                title="Send"
-                disabled={loading || message.length === 0}
-              >
-                {loading ? <CircularProgress /> : <SendIcon />}
-              </IconButton>
-            }
-          />
-          {error && (
-            <FormHelperText sx={{ color: 'danger.500' }}>
-              {error}
-            </FormHelperText>
-          )}
-        </FormControl>
-      </Box>
-    </form>
-  )
-}
-
-const MessageFormWithRef = forwardRef(MessageForm)
-
 function AdminChatView() {
-  const formRef = useRef<HTMLFormElement>(null)
   const { threadId } = useParams<{ threadId: string }>()
-  const chat = useChat(Number(threadId))
 
-  // Scroll to the bottom of the chat when new messages arrive
-  useEffect(() => {
-    formRef.current?.scrollIntoView({ behavior: 'auto' })
-  }, [chat.messages])
-
-  return (
-    <>
-      <MessageContainer thread={threadId} />
-    </>
-  )
+  return <MessageContainer threadId={Number(threadId)} />
 }
 
 export default AdminChatView
