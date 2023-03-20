4dba64ed0356e63d56964d31167378f523734c98
john-royal <34844819+john-royal@users.noreply.github.com>
2023-03-19 22:57:12 -0400

    feat(backend): try sending message from email webhook

--- backend/src/controllers/webhooks.ts
+++ backend/src/controllers/webhooks.ts
@@ -22,9 +22,37 @@ export const ses: RequestHandler = async (req, res) => {
     }
 
     const data = await s3.send(new GetObjectCommand(params))
-    console.dir(data, { depth: null })
+    const result = (await data.Body?.transformToString()) ?? ''
 
-    // create message
+    // extract the thread ID (based on recipient email address, e.g. if the email was sent to 99@example.com, then thread ID is 99)
+    const threadIdRegex = /(\d+)@connecto.johnmroyal\.com/
+    const threadIdMatch = result.match(threadIdRegex)
+    const threadId = threadIdMatch ? parseInt(threadIdMatch[1], 10) : null
+
+    // retrieve the user (based on sender email address)
+    const senderEmailRegex = /From: .*?<(.+?)>/
+    const senderEmailMatch = result.match(senderEmailRegex)
+    const senderEmail = senderEmailMatch ? senderEmailMatch[1] : null
+    const user = await prisma.user.findUnique({ where: { email: senderEmail } })
+
+    // retrieve message content
+    const messageContentRegex =
+      /Content-Type: text\/plain;[\s\S]*?\n\n([\s\S]*?)\n\n--/
+    const messageContentMatch = result.match(messageContentRegex)
+    const messageContent = messageContentMatch
+      ? messageContentMatch[1].trim()
+      : null
+
+    // create the message
+    const newMessage = await prisma.message.create({
+      data: {
+        content: messageContent,
+        thread: { connect: { id: threadId } },
+        user: { connect: { id: user?.id } }
+      },
+      include: { user: true }
+    })
+    console.log(newMessage)
   }
 
   res.status(200).send({ success: true })
