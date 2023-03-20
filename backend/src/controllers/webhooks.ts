7f4c103d58cbdeceb19b057c1de8a484ceea3d1b
john-royal <34844819+john-royal@users.noreply.github.com>
2023-03-19 23:30:54 -0400

    fix(backend): refine email parsing

--- backend/src/controllers/webhooks.ts
+++ backend/src/controllers/webhooks.ts
@@ -5,6 +5,43 @@ import { s3 } from '../lib/aws'
 import prisma from '../lib/prisma'
 import io from '../lib/socket'
 
+const getThreadId = (email: string): number | null => {
+  const threadIdRegex = /(\d+)@connecto.johnmroyal\.com/
+  const threadIdMatch = email.match(threadIdRegex)
+  return threadIdMatch ? parseInt(threadIdMatch[1], 10) : null
+}
+
+const getSenderEmail = (email: string): string | null => {
+  const senderEmailRegex = /From: .*?<(.+?)>/
+  const senderEmailMatch = email.match(senderEmailRegex)
+  return senderEmailMatch ? senderEmailMatch[1] : null
+}
+
+const getMessageContent = (email: string): string | null => {
+  const messageContentRegex =
+    /Content-Type: text\/plain;[\s\S]*?\r?\n\r?\n([\s\S]*?)(?=\r?\n\r?\n--)/
+
+  const match = email.match(messageContentRegex)
+  if (!match?.[1]) return null
+
+  const recentReply = match[1]
+
+  // Split the content into lines
+  const lines = recentReply.split(/\r?\n/)
+
+  // Filter out the lines that belong to the quoted text
+  const filteredLines = lines.filter((line) => {
+    return (
+      !line.startsWith('>') &&
+      !line.startsWith('On ') &&
+      !line.match(/.*@.*\..*> wrote:/)
+    )
+  })
+
+  // Join the filtered lines back together
+  return filteredLines.join('\n')
+}
+
 export const ses: RequestHandler = async (req, res) => {
   const body = JSON.parse(req.body)
 
@@ -24,23 +61,9 @@ export const ses: RequestHandler = async (req, res) => {
     const data = await s3.send(new GetObjectCommand(params))
     const result = (await data.Body?.transformToString()) ?? ''
 
-    // extract the thread ID (based on recipient email address, e.g. if the email was sent to 99@example.com, then thread ID is 99)
-    const threadIdRegex = /(\d+)@connecto.johnmroyal\.com/
-    const threadIdMatch = result.match(threadIdRegex)
-    const threadId = threadIdMatch ? parseInt(threadIdMatch[1], 10) : null
-
-    // retrieve the user (based on sender email address)
-    const senderEmailRegex = /From: .*?<(.+?)>/
-    const senderEmailMatch = result.match(senderEmailRegex)
-    const senderEmail = senderEmailMatch ? senderEmailMatch[1] : null
-
-    // retrieve message content
-    const messageContentRegex =
-      /Content-Type: text\/plain;[\s\S]*?\r?\n\r?\n([\s\S]*?)(?=\r?\n\r?\nOn.*\d{4}.*<.*> wrote:|\r?\n\r?\n--)/
-    const messageContentMatch = result.match(messageContentRegex)
-    const messageContent = messageContentMatch
-      ? messageContentMatch[1].trim()
-      : null
+    const threadId = getThreadId(result)
+    const senderEmail = getSenderEmail(result)
+    const messageContent = getMessageContent(result)
 
     if (threadId == null || !senderEmail || !messageContent) {
       return res.status(400).send({ success: false })
