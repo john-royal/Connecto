412eee5998ddac9b5de7a3a98b43b57762589e41
john-royal <34844819+john-royal@users.noreply.github.com>
2023-03-21 13:42:04 -0400

    fix(frontend): resolve crash on thread creation

--- frontend/src/routes/customers/ChatView.tsx
+++ frontend/src/routes/customers/ChatView.tsx
@@ -1,25 +1,20 @@
-import useSWR from 'swr'
+import { useEffect, useState } from 'react'
+import { redirect } from 'react-router-dom'
 import '../../App.css'
 import Header from '../../components/Header'
 import LoadingView from '../../components/LoadingView'
 import MessageContainer from '../../components/MessageContainer'
 
+interface Thread {
+  id: number
+}
+
 function ChatView() {
-  const { data: thread } = useSWR('/api/threads', async (url) => {
-    const { threads } = await (await fetch(url)).json()
-    if (threads.length > 0) {
-      return threads[0]
-    }
-    const newThreadsResponse = await fetch('/api/threads', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: '{}'
-    })
-    const { thread } = await newThreadsResponse.json()
-    return thread
-  })
+  const [thread, setThread] = useState<Thread | null>(null)
+
+  useEffect(() => {
+    void loadOrCreateThread().then(setThread)
+  }, [])
 
   if (thread == null) {
     return <LoadingView />
@@ -33,6 +28,26 @@ function ChatView() {
       </div>
     </>
   )
+}
+
+const loadOrCreateThread = async () => {
+  const existingThreadsResponse = await fetch('/api/threads')
+  if (existingThreadsResponse.status === 401) {
+    return redirect('/sign-in?redirect=/chat')
+  }
+  const { threads } = await existingThreadsResponse.json()
+  if (threads.length > 0) {
+    return threads[0]
+  }
+  const newThreadsResponse = await fetch('/api/threads', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: '{}'
+  })
+  const { thread } = await newThreadsResponse.json()
+  return thread
 }
 
 export default ChatView
