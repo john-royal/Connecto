aa71d6ff3014e287f6db92842c9f4835782b1485
john-royal <34844819+john-royal@users.noreply.github.com>
2023-03-24 14:33:13 -0400

    feat: fine tune gpt prompts

--- backend/src/lib/completions.ts
+++ backend/src/lib/completions.ts
@@ -39,16 +39,19 @@ const generateBotReply = async ({
   const prompt = `You’re ${BOT_USER_NAME}, a customer service chatbot that is part of the Connecto messaging platform.
   
   Your primary function is to automate routine greetings and data collection to help human representatives work more efficiently.
-  Keep in mind that you cannot directly help customers, but rather gather information and transfer them to a representative.
+  However, you can also answer questions that do not require access to customer data, such as basic product information and troubleshooting.
+  For all other inquiries, gather the customer’s information and transfer them to a representative.
   
   Our customer service chat is used for queries such as help with a product and resolving order issues.
   Avoid being vague, controversial, or off-topic. If the customer says something off-topic, say that you don’t understand and transfer them to a representative.
   Never ask the customer for information we already have on file, such as their name, email address, or phone number.
   Always be polite, friendly, and concise.
 
-  The customer's name is ${
-    customer.name
-  }. The current date and time is ${new Date().toLocaleString()}. Please greet the customer and respond to their messages accordingly.
+  The customer’s name is ${customer.name}. Their email address is ${
+    customer.email
+  } and their phone number is ${
+    customer.phone
+  }.The current date and time is ${new Date().toLocaleString()}. Please greet the customer and respond to their messages accordingly.
   
   Here is an example conversation:
   
@@ -99,39 +102,41 @@ const key = ({
 }
 
 const generateReplySuggestions = async (
-  { id, messages }: ThreadWithCustomerMessages,
+  { id, customer, messages }: ThreadWithCustomerMessages,
   admin: boolean
 ): Promise<string[]> => {
   console.log('generating reply suggestions...')
   const cached = cache.get(key({ id, messages, admin }))
   if (cached != null) return cached
-  let prompt = ''
+  let prompt = `
+  The following is a conversation between a customer and a customer service representative.
+  The customer’s name is ${customer.name}. Their email address is ${customer.email} and their phone number is ${customer.phone}.
+  Please use the following conversation to generate suggested replies, keeping in mind the following guidelines:
+  - Our customer service chat is used for queries such as help with a product and resolving order issues.
+  - Your suggestions should be relevant to the customer’s query. Avoid being vague, controversial, or off-topic.
+  - Your suggestion is a reply. Avoid simply repeating the previous message.
+  - Your suggestions should be polite but concise — preferably no more than one sentence.
+  
+  Here is the conversation:`
+  prompt +=
+    messages
+      .map(
+        ({ content, user }) =>
+          `${user.id === 0 ? 'Bot' : user.name} (${
+            user.isAdmin ? 'representative' : 'customer'
+          }): ${content}`
+      )
+      .join('\n') + '\n\n\n'
   if (admin) {
-    prompt =
-      'You’re a customer service representative, and you’re using Connecto’s customer service chat to help customers with their orders.'
+    prompt += `Please suggest up to 3 replies that the representative can use.
+    Your suggestions must be appropriate for a human customer support representative. Please note that representatives must be able to answer all questions and cannot transfer the customer to another representative.
+    Provide your result as a JSON array of strings. For example: ["Sure thing! Please hold on while I look up your order.", "Is there anything else I can help you with?", "I'll be right back with your order status."]`
   } else {
-    prompt =
-      'You’re a customer, and you’re using Connecto’s customer service chat to get help with your order.'
+    prompt += `Please suggest up to 3 replies that the customer can use.
+    Provide your result as a JSON array of strings. For example: ["I’d like to change my shipping address.", "What is your return policy?", "How do I cancel my subscription?"]`
   }
-  prompt += ` Please provide up to three suggestions for the next message.
-  
-  Our customer service chat is used for queries such as help with a product and resolving order issues.
-  Your suggestions should be polite but concise — preferably no more than one sentence.
-  Your suggestions must be relevant as replies to the previous message. Avoid simply repeating or continuing the previous message.
-  
-  Here is the conversation:
-  ${messages
-    .map(
-      ({ content, user }) =>
-        `${user.isAdmin ? 'Representative' : 'Customer'}: ${content}`
-    )
-    .join('\n')}
-  
-  Format your suggestions as JSON array of strings. For example, ["I’d like to change my shipping address.", "What is your return policy?", "How do I cancel my subscription?"]
-  
-  Feel free to return fewer than 3 suggestions if you can’t think of any more. If you can’t think of any suggestions, return an empty array.
-      
-  Suggestions:`
+  prompt +=
+    'If you are unable to generate any suggestions, please return an empty array.'
   try {
     const response = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
